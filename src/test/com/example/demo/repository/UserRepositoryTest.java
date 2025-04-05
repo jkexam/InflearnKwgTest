8b5567abe4d455a55415e71041d0c0ba1e1e673d
JKOOGI-HP\jkoog <jkoogi@naver.com>
2025-04-05 20:06:35 +0900

    * 1.2.1 테스트클래스 파일 추가 - 테스트설정파일 추가 - 연결(저장) 테스트 추가 - 정상조회 테스트 추가 - 결과없음 테스트 추가

---
+++ src/test/com/example/demo/repository/UserRepositoryTest.java
@@ -0,0 +1,75 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.UserStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+//@SpringBootTest //Junit5에서 생략 가능
+//@ExtendWith(SpringExtension.class) //DataJpaTest에 내장
+//@TestPropertySource("classpath:test-application.properties") //기본설정
+@DataJpaTest(showSql = true)
+class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    void UserRepository연결확인(){
+        //given
+        UserEntity userEntity = new UserEntity();
+        userEntity.setEmail("jkoogibook@gmail.com");
+        userEntity.setAddress("inchun");
+        userEntity.setNickname("jkoogi");
+        userEntity.setStatus(UserStatus.ACTIVE);
+        userEntity.setCertificationCode("34cebab6-0f90-4ae1-b264-9cea6482ba1a");
+
+        //when
+        UserEntity result = userRepository.save(userEntity);
+
+        //then
+        assertThat(result.getId()).isNotNull();
+    }
+
+    @Test
+    void findByIdAndStatus사용자정보조회(){
+        //given
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(1L);
+        userEntity.setEmail("jkoogibook@gmail.com");
+        userEntity.setAddress("inchun");
+        userEntity.setNickname("jkoogi");
+        userEntity.setStatus(UserStatus.ACTIVE);
+        userEntity.setCertificationCode("34cebab6-0f90-4ae1-b264-9cea6482ba1a");
+
+        //when
+        userRepository.save(userEntity);
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1L, UserStatus.ACTIVE);
+
+        //then
+        assertThat(result.isPresent()).isTrue();
+    }
+    @Test
+    void findByIdAndStatus사용자정보조회_결과없음(){
+        //given
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(1L);
+        userEntity.setEmail("jkoogibook@gmail.com");
+        userEntity.setAddress("inchun");
+        userEntity.setNickname("jkoogi");
+        userEntity.setStatus(UserStatus.ACTIVE);
+        userEntity.setCertificationCode("34cebab6-0f90-4ae1-b264-9cea6482ba1a");
+
+        //when
+        userRepository.save(userEntity);
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1L, UserStatus.PENDING);
+
+        //then
+        assertThat(result.isPresent()).isFalse();
+        assertThat(result.isEmpty()).isTrue();
+    }
+}
