52f2936f9300970e67619cc97792ebabe0e3269a
JKOOGI-HP\jkoog <jkoogi@naver.com>
2025-04-05 20:39:03 +0900

    * 동시성 오류 조치 - 쿼리 리소스 파일 추가 - 테스트마다 등록한 테스트데이터를 @sql로 실행한 쿼리로 데이터를 한번만 최기화 하여 각테스트에서 사용

--- src/test/java/com/example/demo/repository/UserRepositoryTest.java
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -4,47 +4,25 @@ import com.example.demo.model.UserStatus;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest(showSql = true)
+@Sql("/sql/user-repository-test-data.sql")
 class UserRepositoryTest {
 
     @Autowired
     UserRepository userRepository;
 
-    @Test
-    void UserRepository연결확인(){
-        //given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setEmail("jkoogibook@gmail.com");
-        userEntity.setAddress("inchun");
-        userEntity.setNickname("jkoogi");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("34cebab6-0f90-4ae1-b264-9cea6482ba1a");
-
-        //when
-        UserEntity result = userRepository.save(userEntity);
-
-        //then
-        assertThat(result.getId()).isNotNull();
-    }
 
     @Test
     void findByIdAndStatus사용자정보조회(){
         //given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("jkoogibook@gmail.com");
-        userEntity.setAddress("inchun");
-        userEntity.setNickname("jkoogi");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("34cebab6-0f90-4ae1-b264-9cea6482ba1a");
 
         //when
-        userRepository.save(userEntity);
         Optional<UserEntity> result = userRepository.findByIdAndStatus(1L, UserStatus.ACTIVE);
 
         //then
@@ -53,16 +31,8 @@ class UserRepositoryTest {
     @Test
     void findByIdAndStatus사용자정보조회_결과없음(){
         //given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("jkoogibook@gmail.com");
-        userEntity.setAddress("inchun");
-        userEntity.setNickname("jkoogi");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("34cebab6-0f90-4ae1-b264-9cea6482ba1a");
 
         //when
-        userRepository.save(userEntity);
         Optional<UserEntity> result = userRepository.findByIdAndStatus(1L, UserStatus.PENDING);
 
         //then
@@ -72,16 +42,8 @@ class UserRepositoryTest {
     @Test
     void findByEmailAndStatus사용자정보조회(){
         //given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("jkoogibook@gmail.com");
-        userEntity.setAddress("inchun");
-        userEntity.setNickname("jkoogi");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("34cebab6-0f90-4ae1-b264-9cea6482ba1a");
 
         //when
-        userRepository.save(userEntity);
         Optional<UserEntity> result = userRepository.findByEmailAndStatus("jkoogibook@gmail.com", UserStatus.ACTIVE);
 
         //then
@@ -90,16 +52,8 @@ class UserRepositoryTest {
     @Test
     void findByEmailAndStatus사용자정보조회_결과없음(){
         //given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("jkoogibook@gmail.com");
-        userEntity.setAddress("inchun");
-        userEntity.setNickname("jkoogi");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("34cebab6-0f90-4ae1-b264-9cea6482ba1a");
 
         //when
-        userRepository.save(userEntity);
         Optional<UserEntity> result = userRepository.findByEmailAndStatus("jkoogibook@gmail.com", UserStatus.PENDING);
 
         //then
