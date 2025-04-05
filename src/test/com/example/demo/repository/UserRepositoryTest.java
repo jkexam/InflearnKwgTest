f71ac5c25fb8cc259674f8e7450f06abbe5bce5a
JKOOGI-HP\jkoog <jkoogi@naver.com>
2025-04-05 20:07:48 +0900

    * 1.2.2 두번째 메소드 테스트 추가 - 병렬처리 동시성 오류발생 확인

--- src/test/com/example/demo/repository/UserRepositoryTest.java
+++ src/test/com/example/demo/repository/UserRepositoryTest.java
@@ -72,4 +72,42 @@ class UserRepositoryTest {
         assertThat(result.isPresent()).isFalse();
         assertThat(result.isEmpty()).isTrue();
     }
+
+    @Test
+    void findByEmailAndStatus사용자정보조회(){
+        //given
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(1L);
+        userEntity.setEmail("jkoogibook@gmail.com");
+        userEntity.setAddress("inchun");
+        userEntity.setNickname("jkoogi");
+        userEntity.setStatus(UserStatus.ACTIVE);
+        userEntity.setCertificationCode("34cebab6-0f90-4ae1-b264-9cea6482ba1a");
+
+        //when
+        userRepository.save(userEntity);
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("jkoogibook@gmail.com", UserStatus.ACTIVE);
+
+        //then
+        assertThat(result.isPresent()).isTrue();
+    }
+    @Test
+    void findByEmailAndStatus사용자정보조회_결과없음(){
+        //given
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(1L);
+        userEntity.setEmail("jkoogibook@gmail.com");
+        userEntity.setAddress("inchun");
+        userEntity.setNickname("jkoogi");
+        userEntity.setStatus(UserStatus.ACTIVE);
+        userEntity.setCertificationCode("34cebab6-0f90-4ae1-b264-9cea6482ba1a");
+
+        //when
+        userRepository.save(userEntity);
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("jkoogibook@gmail.com", UserStatus.PENDING);
+
+        //then
+        assertThat(result.isPresent()).isFalse();
+        assertThat(result.isEmpty()).isTrue();
+    }
 }
